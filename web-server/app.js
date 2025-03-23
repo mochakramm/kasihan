a9c4c52d35bafc8c79bc6d0daa62d012441a6f30
renzo <molenakram8@gmail.com>
2025-03-24 06:01:08 +0700

    belajar routing req

--- web-server/app.js
+++ web-server/app.js
@@ -1,15 +1,44 @@
-const http = require('http') 
-
-const requestListener = (req, res) => {
-    res.setHeader('content-type', 'text/html')
-    res.StatusCode = 200
-    res.end('<h1>hallo rumahku!</h1>')
-}
-
-const server = http.createServer(requestListener)
-const port = 5000
-const hostname = 'localhost'
-
-server.listen(port, hostname, () => {
-    console.log(`rumah udah jalan di http://${hostname}:${port}`)
-})
+const http = require('http');
+	 
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+    response.statusCode = 200;
+	 
+	const { method, url } = request;
+	 
+	if(url === '/') {
+	    if(method === 'GET') {
+	        response.end('<h1>Ini adalah homepage</h1>');
+	    } else {
+	        response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+	    }
+	} else if(url === '/about') {
+	    if(method === 'GET') {
+	        response.end('<h1>Halo! Ini adalah halaman about</h1>')
+	    } else if(method === 'POST') {
+	        let body = [];
+	    
+	        request.on('data', (chunk) => {
+	            body.push(chunk);
+	        });
+	 
+	        request.on('end', () => {
+	            body = Buffer.concat(body).toString();
+	            const { name } = JSON.parse(body);
+	            response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+	        });
+	    } else {
+	        response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
+	    }
+	} else {
+	    response.end('<h1>Halaman tidak ditemukan!</h1>');
+	}
+};
+	 
+const server = http.createServer(requestListener);
+	 
+const port = 5000;
+const host = 'localhost';
+server.listen(port, host, () => {
+	console.log(`Server berjalan pada http://${host}:${port}`);
+});
