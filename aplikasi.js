fe7f953bdb4de84fc73e38d65c766bede807fbad
renzo <molenakram8@gmail.com>
2025-03-25 05:08:38 +0700

    native create web server

--- aplikasi.js
+++ aplikasi.js
@@ -1,37 +1,14 @@
 const http = require('http')
 
-const requestListener = (req,res) => {
-    res.setHeader('Content-type', 'text/html')
-    res.statusCode = 200
-    const method = req.method
-    console.log(`reques method: ${method}`)
-
-    if (method === 'GET') {
-        res.end('<h1>hallo GET</h1>')
-    }
-
-    else if (method === 'PUT') {
-        res.end('<h1>hallo PUT</h1>')
-    }
-
-    else if (method === 'POST') {
-        res.end('<h1>hallo POST</h1>')
-    }
-    else if (method === 'DELETE') {
-        res.end('<h1>hallo DELETE</h1>')
-    }
-
-    else {
-        res.statusCode = 400
-        res.end('<h1>perintah tidak dikenali - error 400</h1>')
-    }
+const requestListener = (req, res) => {
+    const {method, url} = req
+    
 }
 
 const server = http.createServer(requestListener)
-
 const port = 5000
 const hostname = 'localhost'
 
 server.listen(port, hostname, () => {
-    console.log(`server is running at http://${hostname}:${port}`)
+    console.log(`server berjalan di http://${hostname}:${port}`)
 })
